cf01ee3c942105c3b69436579c1175b445e0a161
Ethan Hayward <158235631+Guthib-Programmer@users.noreply.github.com>
2024-12-04 08:34:05 +1300

    Users can log in and go to a blank page based on admin perms

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask_session import Session
 import sqlite3
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.after_request
 def add_header(response):
@@ -22,34 +26,60 @@ def index():
     peopleData = [dict(row) for row in rows]
 
     if request.args.get('person'):
-        cur.execute("SELECT people.name, days.date FROM days JOIN people ON days.person_id = people.id WHERE people.id = ?;", request.args.get('person'))
+        person = request.args.get('person')
+        cur.execute("SELECT days.date, p1.name AS oncall, p2.name AS crisis FROM days JOIN people p1 ON days.person_id = p1.id JOIN people p2 ON days.crisis_id = p2.id WHERE days.person_id = ? OR days.crisis_id = ? ORDER BY days.id ASC LIMIT 50", (person,person))
     else:
-        cur.execute("SELECT people.name, days.date FROM days JOIN people ON days.person_id = people.id;")
+        cur.execute("SELECT date, p1.name AS oncall, p2.name AS crisis FROM days JOIN people p1 ON days.person_id = p1.id JOIN people p2 ON days.crisis_id = p2.id ORDER BY days.id ASC LIMIT 50")
     rows = cur.fetchall()
     daysData = [dict(row) for row in rows]
 
     con.close()
 
-    return render_template('index.html', people=peopleData, days=daysData)
+    if session.get("admin") != None:
+        adminPerms = session['admin']
+        if adminPerms == 1:
+            return render_template('index.html', people=peopleData, days=daysData, accountLink="./signin", controlText="Admin", controlLink="./account")
+        else:
+            return render_template('index.html', people=peopleData, days=daysData, accountLink="./signin", controlText="Swaps", controlLink="./account")
+    else:
+        return render_template('index.html', people=peopleData, days=daysData, accountLink="./signin")
 
-@app.route("/admin", methods=["GET", "POST"])
+@app.route("/signin", methods=["GET", "POST"])
 def admin():
     if request.method == "POST":
-        if not request.form.get("password"):
+        if not request.form.get("password") or not request.form.get("name"):
             return "Not all fields have been filled"
         
         enteredPassword = request.form.get("password")
+        enteredName = request.form.get("name")
 
         con = sqlite3.connect("main.db")
         con.row_factory = sqlite3.Row
         cur = con.cursor()
 
-        cur.execute("SELECT id FROM people WHERE password = ? AND admin = 1;", (enteredPassword,))
+        cur.execute("SELECT id, name, password, admin FROM people WHERE name = ? AND password = ?;", (enteredName, enteredPassword))
         rows = cur.fetchall()
         if (len(rows) == 0):
-            return "incorrect password"
-        else:
-            return "correct password"
+            return render_template('signin.html', accountLink="#", message="Incorrect Username or Password")
+        peopleData = [dict(row) for row in rows]
+
+        session['user_id'] = peopleData[0]['id']
+        session['name'] = peopleData[0]['name']
+        session['password'] = peopleData[0]['password']
+        session['admin'] = peopleData[0]['admin']
+
+        return redirect("/")
         
     else:
-        return render_template('admin.html')
+        return render_template('signin.html', accountLink="#")
+    
+@app.route("/account")
+def account():
+    if session.get("admin") != None:
+        adminPerms = session['admin']
+        if adminPerms == 1:
+            return render_template('admin.html')
+        else:
+            return render_template('user.html')
+    else:
+        return redirect("/")
