9213ccff3100cdd172d73e9316c323e4f473c1c6
Ethan Hayward <158235631+Guthib-Programmer@users.noreply.github.com>
2024-12-03 19:43:09 +1300

    I don't know what changes I made, I left and came back

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, url_for, request
 import sqlite3
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 @app.route("/", methods=["GET"])
 def index():
@@ -24,9 +32,24 @@ def index():
 
     return render_template('index.html', people=peopleData, days=daysData)
 
-@app.route("/admin")
+@app.route("/admin", methods=["GET", "POST"])
 def admin():
-    return render_template('admin.html')
+    if request.method == "POST":
+        if not request.form.get("password"):
+            return "Not all fields have been filled"
+        
+        enteredPassword = request.form.get("password")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        con = sqlite3.connect("main.db")
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+
+        cur.execute("SELECT id FROM people WHERE password = ? AND admin = 1;", (enteredPassword,))
+        rows = cur.fetchall()
+        if (len(rows) == 0):
+            return "incorrect password"
+        else:
+            return "correct password"
+        
+    else:
+        return render_template('admin.html')
